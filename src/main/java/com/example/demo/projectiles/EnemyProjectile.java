b2813ade519ceaf7419e7ea2a6fd9a8b46ac609f
Rayzed0024 <hacker977872@gmail.com>
2024-12-02 18:19:08 +0800

    Rearranged all files with new added features

---
+++ src/main/java/com/example/demo/projectiles/EnemyProjectile.java
@@ -0,0 +1,28 @@
+package com.example.demo.projectiles;
+
+/**
+ * Represents projectiles fired by enemies.
+ */
+
+public class EnemyProjectile extends Projectile {
+	
+	private static final String IMAGE_NAME = "enemyFire.png";
+	private static final int IMAGE_HEIGHT = 30;
+	private static final int HORIZONTAL_VELOCITY = -10;
+
+	public EnemyProjectile(double initialXPos, double initialYPos) {
+		super(IMAGE_NAME, IMAGE_HEIGHT, initialXPos, initialYPos);
+	}
+
+	@Override
+	public void updatePosition() {
+		moveHorizontally(HORIZONTAL_VELOCITY);
+	}
+
+	@Override
+	public void updateActor() {
+		updatePosition();
+	}
+
+
+}
---
+++ src/main/java/com/example/demo/utilities/Destructible.java
@@ -0,0 +1,13 @@
+package com.example.demo.utilities;
+
+/**
+ * Interface defining destructible entities.
+ */
+
+public interface Destructible {
+
+	void takeDamage();
+
+	void destroy();
+	
+}
